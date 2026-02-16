f83d91a7cb35edd7c0e830bedf7afba667f2d1e7
Andreas Eriksson <andreas@codered.se>
2026-02-16 10:28:41 +0100

    Initial commit

---
+++ lib/phx_react.ex
@@ -0,0 +1,18 @@
+defmodule PhxReact do
+  @moduledoc """
+  Documentation for `PhxReact`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhxReact.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phx_react_test.exs
@@ -0,0 +1,8 @@
+defmodule PhxReactTest do
+  use ExUnit.Case
+  doctest PhxReact
+
+  test "greets the world" do
+    assert PhxReact.hello() == :world
+  end
+end
